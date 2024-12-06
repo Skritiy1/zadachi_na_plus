1209b14ba3915b1b4bdf2153dd84c0fb48e33524
Skritiy1 <71488104+Skritiy1@users.noreply.github.com>
2024-12-06 16:52:36 +0300

    Update accuracy.py

--- accuracy.py
+++ accuracy.py
@@ -1,12 +1,14 @@
+
 def find_best_split(zeros, crosses):
+    lin = False
     zeros = sorted(zeros)
     crosses = sorted(crosses)
     all_points = sorted(zeros + crosses)
     max_accuracy = best_point = result = left_zeros = 0
+
     right_crosses = len(crosses)
     accuracy = (left_zeros + right_crosses) / (len(zeros) + len(crosses))
     if accuracy > max_accuracy:
-        max_accuracy = accuracy
         best_point = float('-inf')
     for point in all_points:
         left_zeros = sum(1 for x in zeros if x <= point)
@@ -17,6 +19,9 @@ def find_best_split(zeros, crosses):
 
         if accuracy > max_accuracy:
             max_accuracy = accuracy
+            if max_accuracy == 1:
+                lin = True
+
             best_point = point
 
             left_max = max([x for x in zeros if x <= point], default=None)
@@ -26,10 +31,17 @@ def find_best_split(zeros, crosses):
             if left_max is not None and right_min is not None:
                 result = (left_max + right_min) / 2
 
-    return result
+    return result, max_accuracy, lin
 
 r1 = [int(x) for x in input('введите массив с ноликами: ').split()]
 r2 = [int(x) for x in input('введите массив с крестиками: ').split()]
 
-print(find_best_split(r1, r2))
+if find_best_split(r1, r2)[1] == True:
+    print('Линейно разделимы')
+else:
+    print('Линейно не разделимы')
+print(f'Точка - {find_best_split(r1, r2)[0]}')
+print(f'accuracy - {find_best_split(r1, r2)[1]}')
+
+
 
